21550b8352ed5c2111f30dd36754658321c481d3
aspcarlos <carlosmon2004@gmail.com>
2022-12-20 08:53:15 +0100

    añadir ficheros ejemplo

---
+++ ProyectoGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("EL PRESI HAY COMO ME DUELE EL CORAZON POR EL PRESI AYAYAYAYAYAYA");
+        }
+    }
+}
